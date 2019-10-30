56e94741ee13f9fb59c6cc3a37b8b061b7d7a176
Carlos Fournier <cmfournier@gmail.com>
2019-10-29 21:01:38 -0400

    added required functionality per instructions

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TechJobsConsole
 {
@@ -63,7 +64,8 @@ namespace TechJobsConsole
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
                     }
                     else
                     {
@@ -118,7 +120,25 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            //List<Dictionary<string, string>> somejobs = JobData.FindAll();
+            if (someJobs.Count == 0)
+            {
+                Console.WriteLine("Not found");
+                    }
+            else
+            {
+                foreach (Dictionary<string, string> job in someJobs)
+                {
+                    Console.WriteLine("\n" + "****");
+
+                    foreach (KeyValuePair<string, string> item in job)
+                    {
+                        Console.WriteLine("{0}: {1}", item.Key, item.Value);
+
+                    }
+                    Console.WriteLine("****");
+                }
+            }
         }
     }
 }
